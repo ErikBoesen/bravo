01d56e58517430e36e7fe9c7d36b012fb7c128bd
carter.fendley <carter.fendley@gmail.com>
2016-10-27 09:32:16 -0400

    Root dir will work regardless where you execute server.rb from

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require "webrick"
 
 # Open a basic webserver.
-root = File.expand_path "./public"
+root = File.expand_path "../public", __FILE__
 server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
 trap "INT" do server.shutdown end
 
