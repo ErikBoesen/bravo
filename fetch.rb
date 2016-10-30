efd57fa9d502bdada50564b0a65a9173e5c129ba
ErikBoesen <me@erikboesen.com>
2016-10-30 18:56:54 -0400

    Matches are now in order by type.

--- fetch.rb
+++ fetch.rb
@@ -1,8 +1,12 @@
 require "tba"
 require "json"
 
+print "Enter team number to track: "
+# Take inputted team number and convert it to an integer
+team = gets.chomp.to_i
+
 # Initialize TBA API
-tba = TBA.new("frc1418:bravo:v0.1.0")
+tba = TBA.new("frc#{team}:bravo:v0.1.0")
 
 print "Enter event ID: "
 # Recieve inputted event ID
@@ -10,21 +14,63 @@ event_id = gets.chomp
 # Fetch event data from The Blue Alliance
 event = tba.get_event(event_id)
 
-print "Enter team number to track: "
-# Take inputted team number and convert it to an integer
-team = gets.chomp.to_i
 
 puts "Getting Team #{team}'s matches at event #{event_id}..."
 # Fetch matches that the team is in at this competition
 matches = tba.get_team_matches(team, event_id)
 
-puts "#{matches.length} matches fetched. Building team list... (this could take a while)"
+# TODO: There's almost certainly a better way to sort the array by match type.
+# This method feels very heavy and innefficient. Find a better system.
+# Perhaps something clever with matches.sort_by.
+
+# Create new arrays to hold elimination matches
+matches_qf = Array.new
+matches_sf = Array.new
+matches_f = Array.new
+
+# Create array to hold elimination match indices in match array
+matches_elim = Array.new
+
+# Go through the match list.
+matches.each_with_index do |match, i|
+    # If a match is a quarterfinal, semifinal, or final match,
+    # add it to the appropriate list and mark it for removal later on.
+    case match["comp_level"]
+        when "qf"
+            # Add to quarterfinal match list
+            matches_qf.push(match)
+            # Add index of match to list of indices we created above
+            matches_elim.push(i)
+        when "sf"
+            matches_sf.push(match)
+            matches_elim.push(i)
+        when "f"
+            matches_f.push(match)
+            matches_elim.push(i)
+    end
+end
+
+# Go backwards through the list of indices of elimination matches.
+matches_elim.reverse_each do |i|
+    # Remove each of them.
+    matches -= [matches[i]]
+end
+
+# Sort qualification matches by number.
+matches.sort_by! do |match|
+    match["match_number"]
+end
+
+# Combine qual matches with quarters, semis, and finals; but this time in order.
+matches += matches_qf + matches_sf + matches_f
+
+puts "#{matches.length} matches fetched and processes. Building team list... (this could take a while)"
 # Make a new array to hold all the teams that played in those matches
 teams = Array.new
 
 # Go through each match and add all teams that played in those matches
 matches.each do |match|
-    # TODO: Dot notation?
+    # TODO: Dot notation/symbolization?
 
     # Add red teams
     teams += match["alliances"]["red"]["teams"]
@@ -44,8 +90,6 @@ teams.each do |team|
     # (Turn string form [i.e. "frc1418"] into integer, i.e. 1418)
     team_data[team] = tba.get_team(team[3..-1].to_i)
 end
-
-# TODO: Move QF, SF, and F matches to the end of the list. Not necessary since they won't normally be available when the fetching is done, but would still be a nice idea.
 
 puts "Storing data..."
 
